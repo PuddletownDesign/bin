9a16631ba9b36579cec5bf1aaf5d126ab3e00c93
Brent <puddletowndesign@gmail.com>
2017-04-26 20:12:36 -0500

    updated biniaries

--- backup-configs.sh
+++ backup-configs.sh
@@ -6,13 +6,13 @@ git add .
 git commit -m "updated biniaries"
 git push origin master
 
-echo "backing up brew"
-cd ~/Documents/Dev/Brew
-brew list > packages.list
-brew cask list > casks-all.list
-git add .
-git commit -m "updated brew packages"
-git push origin mac
+# echo "backing up brew"
+# cd ~/Documents/Dev/Brew
+# brew list > packages.list
+# brew cask list > casks-all.list
+# git add .
+# git commit -m "updated brew packages"
+# git push origin mac
 
 echo "backing up git"
 cd ~/Documents/Dev/Git/
